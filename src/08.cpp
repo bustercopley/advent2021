181ee65c1ce68b852bdab8c77e5d849a4613e4d9
Buster <buster@buster.me.uk>
2021-12-08 15:41:57 +0000

    Day 8: direct approach

--- src/08.cpp
+++ src/08.cpp
@@ -3,20 +3,9 @@
 auto regex = re::regex(R"(^(\d+),(\d+)$|^([a-z ]+)[|]([a-z ]+)$)");
 auto regex2 = re::regex(R"(([a-z]+)(.*))");
 
-const std::map<ss, int> canonical = {
-  {"abcefg", 0},
-  {"cf", 1},
-  {"acdeg", 2},
-  {"acdfg", 3},
-  {"bcdf", 4},
-  {"abdfg", 5},
-  {"abdefg", 6},
-  {"acf", 7},
-  {"abcdefg", 8},
-  {"abcdfg", 9},
-};
-
-const std::map<sz, int> easy = {{2, 1}, {4, 4}, {3, 7}, {7, 8}};
+bool contains(auto range, auto element) {
+  return std::ranges::find(range, element) != std::end(range);
+}
 
 void parts(std::istream &stream, int part) {
   bool test = false;
@@ -39,87 +28,90 @@ void parts(std::istream &stream, int part) {
 
   if (part == 1) {
     for (const auto &[input_ignored, output]: report) {
-      for (auto [s]: split(output, regex2)) {
-        if (auto iter = easy.find(size(s)); iter != end(easy)) { ++result; }
+      for (auto [digit]: split(output, regex2)) {
+        switch (size(digit)) {
+        case 2:
+        case 3:
+        case 4:
+        case 7: ++result;
+        }
       }
     }
   } else {
     for (const auto &[input, output]: report) {
-      ss digits[10];
+      std::map<ss, int> decode;
+      std::vector<ss> digits;
+      int counts[7]{};
+      char b{}, c{}, e{}, f{};
+      ss s1, s2;
 
-      // Decode the four easy digits
-      sz easy_count = 0;
-      for (auto [word]: split(input, regex2)) {
-        if (auto iter = easy.find(size(word)); iter != end(easy)) {
-          if (empty(digits[iter->second])) {
-            std::string s{word};
-            std::ranges::sort(s);
-            digits[iter->second] = s;
-            if (++easy_count == 4) { break; }
+      for (auto [digit]: split(input, regex2)) {
+        for (auto segment: digit) {
+          ++counts[segment - 'a'];
+        }
+        ss canonical{digit};
+        std::ranges::sort(canonical);
+        digits.push_back(canonical);
+        switch (size(digit)) {
+        case 2: decode[s1 = canonical] = 1; break;
+        case 3: decode[canonical] = 7; break;
+        case 4: decode[canonical] = 4; break;
+        case 7: decode[canonical] = 8; break;
+        }
+      }
+
+      // e is the only segment contained in 4 digits
+      // f is the only segment contained in 9 digits
+      for (char segment = 'a'; auto count: counts) {
+        if (count == 4) { e = segment; }
+        if (count == 9) { f = segment; }
+        ++segment;
+      }
+
+      // 2 is the only digit not containing segment f
+      for (auto digit: digits) {
+        if (!contains(digit, f)) { decode[s2 = digit] = 2; }
+      }
+
+      // b is the only segment not contained in 2 except for f
+      // c is the only segment contained in 1 except for f
+      for (auto segment: ss{"abcdefg"}) {
+        if (segment != f) {
+          if (!contains(s2, segment)) { b = segment; }
+          if (contains(s1, segment)) { c = segment; }
+        }
+      }
+
+      // 5 is the only 5-segment digit containing b
+      // 3 is the only 5-segment digit not containing b except for 2
+      // 6 is the only 6-segment digit not containing c
+      // 9 is the only 6-segment digit not containing e
+      // 0 is the last remaining digit
+      for (auto digit: digits) {
+        if (size(digit) == 5) {
+          if (contains(digit, b)) {
+            decode[digit] = 5;
+          } else if (digit != s2) {
+            decode[digit] = 3;
+          }
+        } else if (size(digit) == 6) {
+          if (!contains(digit, c)) {
+            decode[digit] = 6;
+          } else if (!contains(digit, e)) {
+            decode[digit] = 9;
+          } else {
+            decode[digit] = 0;
           }
         }
       }
 
-      assert(easy_count == 4);
-      assert(!empty(digits[1]));
-      assert(!empty(digits[4]));
-      assert(!empty(digits[7]));
-      assert(!empty(digits[8]));
-
-      // Get a possible ordering of the segments
-      std::string segments(7, '\0');
-      segments[0] = digits[7][digits[7].find_first_not_of(digits[1])];
-      segments[2] = digits[1][0];
-      segments[5] = digits[1][1];
-      for (auto c: digits[4]) {
-        if (c != segments[2] && c != segments[5]) {
-          segments[segments[1] ? 3 : 1] = c;
-        }
+      int value = 0;
+      for (auto [digit]: split(output, regex2)) {
+        ss canonical{digit};
+        std::ranges::sort(canonical);
+        value = 10 * value + decode[canonical];
       }
-      for (auto c: digits[8]) {
-        if (c != segments[0] && c != segments[1] && c != segments[2] &&
-          c != segments[3] && c != segments[5]) {
-          segments[segments[4] ? 6 : 4] = c;
-        }
-      }
-
-      // Check permutations
-      for (int permutation = 0; permutation != 8; ++permutation) {
-        if ((permutation & 0) == 0) std::swap(segments[2], segments[5]);
-        if ((permutation & 1) == 1) std::swap(segments[1], segments[3]);
-        if ((permutation & 3) == 3) std::swap(segments[4], segments[6]);
-        // Build cross-reference map from segment-set to digit
-        std::string segments_xref(7, '\0');
-        for (int index = 0; auto c: segments) {
-          segments_xref[c - 'a'] = static_cast<char>('a' + index);
-          ++index;
-        }
-        // Is this it?
-        std::map<ss, int> digits_xref;
-        for (auto words: {input, output}) {
-          for (auto [word]: split(words, regex2)) {
-            // Apply permutation to segment-set
-            ss s{word};
-            for (auto &c: s) {
-              c = segments_xref[c - 'a'];
-            }
-            std::ranges::sort(s);
-            if (auto iter = canonical.find(s); iter != end(canonical)) {
-              digits_xref[ss{word}] = iter->second;
-            } else {
-              goto next_permutation;
-            }
-          }
-        }
-        for (int place_value = 1000; auto [word]: split(output, regex2)) {
-          result += place_value * digits_xref[ss{word}];
-          place_value /= 10;
-        }
-        goto next_input_line;
-next_permutation:;
-      }
-      assert(false);
-next_input_line:;
+      result += value;
     }
   }
 
