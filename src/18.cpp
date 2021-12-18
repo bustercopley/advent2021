ddeae479928757dd634d8b6dd6fb91285e25e71b
Buster <buster@buster.me.uk>
2021-12-18 09:16:45 +0000

    Day 18: tidy up

--- src/18.cpp
+++ src/18.cpp
@@ -1,5 +1,4 @@
 #include "precompiled.h"
-#include "y-combinator.h"
 
 auto regex = re::regex(R"(^(\d+),(\d+)$|^[][0-9,]+$)");
 using couple = std::array<int, 2>;
@@ -7,175 +6,159 @@ using couple = std::array<int, 2>;
 constexpr sz undef = std::numeric_limits<sz>::max();
 
 struct node {
-  bool terminal;
-  int value;
-  sz left;
-  sz right;
+  using ptr = std::unique_ptr<node>;
+  ll value;
+  ptr children[2];
+  node(ll value): value(value) {}
+  node(ptr &&left, ptr &&right): value(-1) {
+    children[0] = move(left);
+    children[1] = move(right);
+  }
+  bool is_leaf() { return value != -1; }
 };
 
-void parts(std::istream &stream) {
-  bool test = false;
-  ll expected[2]{};
+node::ptr make_node(int value) { return std::make_unique<node>(value); }
 
-  vec<node> nodes;
+node::ptr make_node(node::ptr &&left, node::ptr &&right) {
+  return std::make_unique<node>(move(left), move(right));
+}
 
-  y_combinator read{[&nodes](auto read, auto &range) -> sz {
-    if (range[0] == '[') {
-      range.advance(1);
-      sz left = read(range);
-      assert(range[0] == ',');
-      range.advance(1);
-      sz right = read(range);
-      assert(range[0] == ']');
-      range.advance(1);
-      sz n = size(nodes);
-      nodes.emplace_back(false, 0, left, right);
-      return n;
+node::ptr read(const char *&p) {
+  if ('0' <= *p && *p <= '9') { return make_node(*p++ - '0'); }
+  node::ptr left = read(++p);
+  node::ptr right = read(++p);
+  ++p;
+  return make_node(move(left), move(right));
+}
+
+node *find_if(const node::ptr &p, auto f, int level = 0) {
+  if (f(p.get(), level)) { return p.get(); }
+  if (!p->is_leaf()) {
+    for (auto &child: p->children) {
+      if (auto p = find_if(child, f, level + 1); p) return p;
     }
-    if ('0' <= range[0] && range[0] <= '9') {
-      int value = range[0] - '0';
-      range.advance(1);
-      sz n = size(nodes);
-      nodes.emplace_back(true, value, undef, undef);
-      return n;
-    }
-    std::abort();
-  }};
+  }
+  return nullptr;
+}
 
-  auto reduce = [&nodes](sz root) {
-    y_combinator find_if{
-      [&nodes](auto find_if, sz iter, auto f, int level = 0) -> sz {
-        if (f(iter, level)) return iter;
-        if (nodes[iter].terminal) return undef;
-        if (auto p = find_if(nodes[iter].left, f, level + 1); p != undef) {
-          return p;
-        }
-        return find_if(nodes[iter].right, f, level + 1);
-      }};
+node::ptr copy(const node::ptr &p) {
+  if (!p->is_leaf()) {
+    return make_node(copy(p->children[0]), copy(p->children[1]));
+  } else {
+    return make_node(p->value);
+  }
+}
 
-    while (true) {
-      sz prev = undef;
-      sz exploder = undef;
-
-      if (auto next = find_if(root,
-            [&nodes, &exploder, &prev](sz iter, int level) {
-              if (exploder == undef && level == 4 && !nodes[iter].terminal) {
-                assert(nodes[nodes[iter].left].terminal);
-                assert(nodes[nodes[iter].right].terminal);
-                exploder = iter;
-                return false;
-              }
-              if (exploder != undef) {
-                return iter != nodes[exploder].left &&
-                  iter != nodes[exploder].right && nodes[iter].terminal;
-              }
-              if (nodes[iter].terminal) { prev = iter; }
+void reduce(const node::ptr &p) {
+  while (true) {
+    node *prev = nullptr;
+    node *exploder = nullptr;
+    if (node *next = find_if(p,
+          [&prev, &exploder](node *p, int level) {
+            if (!exploder && level == 4 && !p->is_leaf()) {
+              exploder = p;
               return false;
-            });
-          exploder != undef) {
-        if (prev != undef) {
-          nodes[prev].value += nodes[nodes[exploder].left].value;
-        }
-        if (next != undef) {
-          nodes[next].value += nodes[nodes[exploder].right].value;
-        }
-        nodes[exploder].terminal = true;
-        nodes[exploder].value = 0;
-      } else if (auto splitter = find_if(root,
-                   [&nodes](sz iter, int) {
-                     return nodes[iter].terminal && nodes[iter].value >= 10;
-                   });
-                 splitter != undef) {
-        auto value = nodes[splitter].value;
-        nodes[splitter].terminal = false;
-        nodes[splitter].value = 0;
-        nodes[splitter].left = size(nodes);
-        nodes.emplace_back(true, value / 2, undef, undef);
-        nodes[splitter].right = size(nodes);
-        nodes.emplace_back(true, (value + 1) / 2, undef, undef);
-      } else {
-        break;
-      }
+            }
+            if (exploder) {
+              return p != exploder->children[0].get() &&
+                p != exploder->children[1].get() && p->is_leaf();
+            }
+            if (p->is_leaf()) { prev = p; }
+            return false;
+          });
+        exploder) {
+      if (prev) prev->value += exploder->children[0]->value;
+      if (next) next->value += exploder->children[1]->value;
+      exploder->value = 0;
+      exploder->children[0] = nullptr;
+      exploder->children[1] = nullptr;
+    } else if (node *splitter =
+                 find_if(p, [](node *p, int) { return p->value >= 10; })) {
+      splitter->children[0] = make_node(splitter->value / 2);
+      splitter->children[1] = make_node((splitter->value + 1) / 2);
+      splitter->value = -1;
+    } else {
+      break;
     }
-  };
+  }
+}
 
-  y_combinator copy{[&nodes](auto copy, sz index) -> sz {
-    sz result = size(nodes);
-    nodes.push_back(nodes[index]);
-    if (!nodes[result].terminal) {
-      nodes[result].left = copy(nodes[result].left);
-      nodes[result].right = copy(nodes[result].right);
-    }
-    return result;
-  }};
-
-  auto add = [&nodes, copy, reduce](sz a, sz b) {
-    sz result = size(nodes);
-    nodes.emplace_back(false, 0, undef, undef);
-    nodes[result].left = copy(a);
-    nodes[result].right = copy(b);
+node::ptr add(const node::ptr &a, const node::ptr &b) {
+  if (!a.get()) {
+    return copy(b);
+  } else {
+    node::ptr result = make_node(copy(a), copy(b));
     reduce(result);
     return result;
-  };
-
-  vec<sz> numbers;
-
-  for (ss line; std::getline(stream, line);) {
-    if (auto m = match(regex, line)) {
-      if (matched(m, 1)) {
-        test = true;
-        expected[0] = string_to<ll>(match_string(m, 1));
-        expected[1] = string_to<ll>(match_string(m, 2));
-      } else {
-        auto range = std::ranges::subrange(line);
-        numbers.push_back(read(range));
-      }
-    }
   }
+}
 
-  y_combinator magnitude{[&nodes](auto magnitude, sz iter) -> ll {
-    if (nodes[iter].terminal) { return nodes[iter].value; }
-    return 3 * magnitude(nodes[iter].left) + 2 * magnitude(nodes[iter].right);
-  }};
-
-  assert(!empty(numbers));
-
-  sz accumulator = undef;
-  for (auto number: numbers) {
-    if (accumulator == undef) {
-      accumulator = number;
-    } else {
-      accumulator = add(accumulator, number);
-    }
-  }
-
-  ll part2 = std::numeric_limits<ll>::min();
-  for (auto a: numbers) {
-    for (auto b: numbers) {
-      if (a != b) { part2 = std::max(part2, magnitude(add(a, b))); }
-    }
-  }
-
-  ll result[2] = {magnitude(accumulator), part2};
-
-  if (!test) {
-    std::cout << "Part 1 result " << result[0] << "\nPart 2 result "
-              << result[1] << std::endl;
+ll magnitude(const node::ptr &p) {
+  if (p->is_leaf()) {
+    return p->value;
   } else {
-    for (int part: {1, 2}) {
-      if (result[part - 1] != expected[part - 1]) {
-        std::cout << "Part " << part << " fail, result " << result[part - 1]
-                  << ", expected " << expected[part - 1] << std::endl;
-      }
-    }
+    return 3 * magnitude(p->children[0]) + 2 * magnitude(p->children[1]);
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream, const node::ptr &p) {
+  if (p->is_leaf()) {
+    return stream << p->value;
+  } else {
+    return stream << "[" << p->children[0] << "," << p->children[1] << "]";
   }
 }
 
 int main() {
   std::cout << "Day 18" << std::endl;
-  for (auto filename: {"test/18", "input/18"}) {
-    if (std::ifstream stream(filename); stream) { parts(stream); }
-  }
 
+  for (auto filename: {"test/18", "input/18"}) {
+    if (std::ifstream stream(filename); stream) {
+      bool test = false;
+      ll expected[2];
+      ll results[2]{0, 0};
+      vec<node::ptr> numbers;
+
+      for (ss line; std::getline(stream, line);) {
+        if (auto m = match(regex, line)) {
+          if (matched(m, 1)) {
+            test = true;
+            expected[0] = string_to<ll>(match_string(m, 1));
+            expected[1] = string_to<ll>(match_string(m, 2));
+          } else {
+            const char *p = data(line);
+            numbers.push_back(read(p));
+          }
+        }
+      }
+
+      node::ptr accumulator = nullptr;
+      for (auto &n: numbers) {
+        accumulator = add(accumulator, n);
+      }
+      results[0] = magnitude(accumulator);
+
+      for (auto &a: numbers) {
+        for (auto &b: numbers) {
+          if (a != b) {
+            results[1] = std::max(results[1], magnitude(add(a, b)));
+          }
+        }
+      }
+
+      if (test) {
+        for (int part: {1, 2}) {
+          if (results[part - 1] != expected[part - 1]) {
+            std::cout << "Part " << part << " fail, result "
+                      << results[part - 1] << ", expected "
+                      << expected[part - 1] << std::endl;
+          }
+        }
+      } else {
+        std::cout << "Part 1 result " << results[0] << "\nPart 2 result "
+                  << results[1] << std::endl;
+      }
+    }
+  }
   return 0;
 }
