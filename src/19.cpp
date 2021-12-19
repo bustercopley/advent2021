dc22907c1c58f2bb6251fa524d2c091c3dd80d08
Buster <buster@buster.me.uk>
2021-12-19 07:59:49 +0000

    Day 19: tidy up and add comments

--- src/19.cpp
+++ src/19.cpp
@@ -13,6 +13,7 @@ struct point {
 
 int G[24][3][3]{};
 void make_G() {
+  // 6 rotations that take (1, 0, 0) to different places
   int H[6][3][3] = {
     {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}},
     {{0, 1, 0}, {0, 0, 1}, {1, 0, 0}},
@@ -21,12 +22,14 @@ void make_G() {
     {{0, -1, 0}, {0, 0, -1}, {1, 0, 0}},
     {{0, 0, -1}, {-1, 0, 0}, {0, 1, 0}},
   };
+  // 4 rotations that fix (1, 0, 0)
   int K[4][3][3] = {
     {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}},
     {{1, 0, 0}, {0, 0, 1}, {0, -1, 0}},
     {{1, 0, 0}, {0, -1, 0}, {0, 0, -1}},
     {{1, 0, 0}, {0, 0, -1}, {0, 1, 0}},
   };
+  // 24 rotations which do an H followed by a K
   for (int m = 0; m != 6; ++m)
     for (int n = 0; n != 4; ++n)
       for (int i = 0; i != 3; ++i)
@@ -40,7 +43,6 @@ void parts(std::istream &stream) {
   int expected[2];
   int result[2]{};
   vec<vec<point>> given;
-  std::map<point, vec<sz>> fixed;
 
   for (ss line; std::getline(stream, line);) {
     if (auto m = match(regex, line)) {
@@ -59,12 +61,19 @@ void parts(std::istream &stream) {
     }
   }
   if (empty(given)) return;
+
+  // Scanner positions relative to scanner 0
   vec<point> scanners(size(given));
 
+  // Map (beacon position relative to scanner 0) to (scanners that can see it)
+  std::map<point, vec<sz>> fixed;
+
+  // Initialize map with beacons seen by scanner 0
   for (const auto &p: given.front()) {
-    fixed.try_emplace(p, vec<sz>{sz(0)});
+    fixed.emplace(p, vec<sz>{0});
   }
 
+  // List of scanners not yet matched up to scanner 0
   std::set<sz> remaining;
   for (sz n = 1; n != size(given); ++n) {
     remaining.insert(n);
@@ -75,23 +84,31 @@ void parts(std::istream &stream) {
       for (const auto &g: G) {
         for (const auto &p: given[n]) {
           for (const auto &[q, ignored]: fixed) {
+            // Suppose:
+            //   p (specified in the frame of scanner n) is the same beacon as
+            //   q (specified in the frame of scanner 0), and
+            //   g is the orientation of scanner 0 (in the frame of scanner n).
+
+            // Find the position of scanner n in the frame of scanner 0.
             point dp{
               g[0][0] * p.x + g[0][1] * p.y + g[0][2] * p.z - q.x,
               g[1][0] * p.x + g[1][1] * p.y + g[1][2] * p.z - q.y,
               g[2][0] * p.x + g[2][1] * p.y + g[2][2] * p.z - q.z,
             };
 
-            // matches[scanner] = { beacon, ... };
-            vec<int> matches(size(given));
+            // For each scanner, how many beacons it can see match one of ours?
+            vec<sz> matches(size(given)); // matches[scanner] = count
 
             vec<point> beacons(size(given[n]));
             for (sz i = 0; i != size(given[n]); ++i) {
               const auto &r = given[n][i];
+              // Find the position of this beacon in the frame of scanner 0.
               beacons[i] = point{
                 g[0][0] * r.x + g[0][1] * r.y + g[0][2] * r.z - dp.x,
                 g[1][0] * r.x + g[1][1] * r.y + g[1][2] * r.z - dp.y,
                 g[2][0] * r.x + g[2][1] * r.y + g[2][2] * r.z - dp.z,
               };
+              // Increment matches
               if (auto iter = fixed.find(beacons[i]); iter != end(fixed)) {
                 for (auto scanner: iter->second) {
                   ++matches[scanner];
@@ -101,11 +118,16 @@ void parts(std::istream &stream) {
 
             for (sz scanner = 0; scanner != size(given); ++scanner) {
               if (matches[scanner] >= 12) {
+                // Found a match. For each beacon scanner n can see, add the
+                // beacon to the map if not already present, and add scanner n
+                // to the list of scanners that can see the beacon.
                 for (const auto &beacon: beacons) {
                   fixed[beacon].push_back(n);
                 }
-                remaining.erase(n);
                 scanners[n] = dp;
+                remaining.erase(n);
+                // Escape from nested for loops and check all the remaining
+                // beacons again.
                 goto next;
               }
             }
@@ -113,7 +135,7 @@ void parts(std::istream &stream) {
         }
       }
     }
-    break;
+    std::abort(); // Unreachable, if input is correct
 next:;
   }
 
